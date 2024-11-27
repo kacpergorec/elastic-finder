c003adfa202c04eca9359e8c64b23a8cee4304ec
kacpergorec <kacpergorec@gmail.com>
2024-11-27 19:13:12 +0100

    feat: add base templates

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,14 +3,17 @@ declare (strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Attribute\Route;
 
-class IndexController
+#[AsController]
+class IndexController extends AbstractController
 {
     #[Route('/')]
     public function index(): Response
     {
-        return new Response('Hello multiverse!');
+        return $this->render('index.html.twig');
     }
 }
